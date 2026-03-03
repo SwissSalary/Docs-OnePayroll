0e40909bf72a36715aaee5a386c727c50d422791
Zech Andersen <zech.andersen@swisssalary.com>
2026-03-03 11:15:15 -0600

    Update branding to be more like OnePayroll / SwissSalary (#16)
    * Update home page, add branding styles
    
    * Update home page
    
    * Change applogourl to return to documentation home
    
    * Fix horizontal scrollbars
    
    * Refer to Business Central full product name
    
    Co-authored-by: Daniel Jovanovic <danjov@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Daniel Jovanovic <danjov@users.noreply.github.com>

--- templates/custom/public/main.js
+++ templates/custom/public/main.js
@@ -1,4 +1,22 @@
 export default {
+    defaultTheme: 'light',
+    iconLinks: [
+        {
+            icon: 'linkedin',
+            href: 'https://www.linkedin.com/company/onepayrollusa',
+            title: 'LinkedIn'
+        },
+        {
+            icon: 'youtube',
+            href: 'https://www.youtube.com/@OnePayrollUSA',
+            title: 'YouTube'
+        },
+        {
+            icon: 'globe',
+            href: 'https://onepayroll.com',
+            title: 'OnePayroll'
+        }
+    ],
     anchors: {
         placement: 'left',
         icon: '\u{F470}'  // Bootstrap Icons: bi-link-45deg
